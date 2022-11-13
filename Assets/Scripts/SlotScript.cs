03a166e5a866f46c7cdbdb4b658b5485acb92eea
BrokAnkle <94972613+BrokAnkle@users.noreply.github.com>
2022-11-13 14:27:32 +0100

    Test completed

--- Assets/Scripts/SlotScript.cs
+++ Assets/Scripts/SlotScript.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class SlotScript : MonoBehaviour
 {
-    //[SerializeField, Range(0, 8), Tooltip("The number of the tile that this slot should hold to win")] int chunkNumber;
-    public TILE_POSITION slotPosition;
-    public TILE_POSITION holdingTile;
-    
+    public TILE_POSITION slotPosition;  //position inside the grid
+    public TILE_POSITION holdingTile;   //position of where should be the holding tile
 }
